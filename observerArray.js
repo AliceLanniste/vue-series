9dcb4cbb47d8f1c9c1139078e6dd98cb392ec439
彭思思 <1399789151@qq.com>
2023-02-24 18:40:26 +0800

    complete object getter/setter

--- observerArray.js
+++ observerArray.js
@@ -40,26 +40,47 @@ class Observer{
 }
 
 function defineReactive(obj,key) {
-    let val = obj[key]
+    const property = Object.getOwnPropertyDescriptor(obj,key)
+    if (property && property.configurable === false) {
+        return;
+      }
+      let val
+    const getter = property.get
+    const setter = property.set
+    if ((!getter || setter) && arguments.length === 2) {
+        val = obj[key]
+    }
+    
     let childObj = observe(val)
     Object.defineProperty(obj,key,{
         get() {
+            const value = getter ? getter.call(obj): val
             console.log('getter trigger',key)
-            return val
+            return value
         },
 
         set(newVal) {
-            if (newVal === val) {
+            const value = getter ? getter.call(obj): val
+            if (newVal === value) {
                 return
             }
 
             console.log('setter trigger',newVal)
-            val = newVal
+            if (setter) {
+                setter.call(obj, newVal);
+            } else {
+                val = newVal
+            }
             childObj = observe(newVal)
         }
     })
 }
 
+
+
+function isObject(obj) {
+    return obj !== null && typeof obj ==='object'
+}
 
 let obj = {
     a: {
@@ -73,11 +94,19 @@ let obj = {
   };
 
 
+//   observe(obj)
+//   console.log(obj.a.aa.aaa = 234)
 
-function isObject(obj) {
-    return obj !== null && typeof obj ==='object'
-}
+let obj2 = {};
+Object.defineProperty(obj2, "a", {
+    get() {
+        return obj2._a;
+    },
+    set(val) {
+        obj2._a = val;
+    },
+});
+
+obj2._a = 123;
 
 
-observe(obj)
-console.log(obj.a.aa.aaa = 234)
