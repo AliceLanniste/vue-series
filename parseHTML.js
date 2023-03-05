848a11898b95b646fd96b4228aa0e9c043fe994e
彭思思 <1399789151@qq.com>
2023-03-05 11:30:21 +0800

    added optimize function

--- parseHTML.js
+++ parseHTML.js
@@ -166,5 +166,51 @@ function parseEndTag(match) {
 }
 
 
+// markStatic
+function optimize(ast) {
+    function isStatic(node) {
+        if (node.type === 2) {
+            return false
+        }
+        if (node.type === 3) {
+            return true
+        }
+        return (!node.if && !node.for);
+    }
+
+    function markStatic(node) {
+        node.static = isStatic(node)
+        if (node.type === 1) {
+            for (let index = 0; index < node.children.length; index++) {
+                const child = node.children[index]
+                markStatic(child)
+                if (!child.static) {
+                    node.static = false
+                }
+            }
+        }
+      
+    }
+
+    function markStaticRoots (node) {
+        if (node.type === 1) {
+            if (node.static && node.children.length && !(
+            node.children.length === 1 &&
+            node.children[0].type === 3
+            )) {
+                node.staticRoot = true;
+                return;
+            } else {
+                node.staticRoot = false;
+            }
+        }
+    }
+
+    markStatic(rootAst);
+    markStaticRoots(rootAst);
+}
+
+
 let rootAst = parse(htmlStr)
+optimize(rootAst)
 console.log('rootAst',rootAst)
